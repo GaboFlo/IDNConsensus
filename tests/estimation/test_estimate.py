dbc10d51a09d399debdd46ddac708f1831d68291
Florian Gaboriaud <florian.gaboriaud@polyconseil.fr>
2022-04-02 16:10:18 +0200

    :umbrella: Add estimation tests (1 camera frame of reference)

---
+++ tests/estimation/test_estimate.py
@@ -0,0 +1,55 @@
+from tabnanny import check
+
+import numpy as np
+import pytest_check as check
+from idnc.estimation.estimate_target_position import (
+    from_txt_label_to_array,
+    labels_to_relative_angles,
+)
+
+
+def test_estimate_fullBox():
+    coord = from_txt_label_to_array("./tests/estimation/test_fullBox.txt")
+    expectedCoordinates = {
+        "x_box_center": 0.5,
+        "y_box_center": 0.5,
+        "width_box": 1,
+        "height_box": 1,
+    }
+    for key in expectedCoordinates:
+        check.is_true(
+            np.all(np.isclose(expectedCoordinates[key], coord[key])),
+            f"Recreate coordinates {coord[key]} while expecting {expectedCoordinates[key]}.",
+        )
+
+    angles = labels_to_relative_angles(expectedCoordinates)
+    horizontalFOVDegree = 70.42
+    expectedAngles = (-horizontalFOVDegree / 2, horizontalFOVDegree / 2)
+    check.is_true(
+        np.all(np.isclose(expectedAngles, angles)),
+        f"Received angles {angles} instead of {expectedAngles}",
+    )
+
+
+def test_estimate_smallerBox():
+    coord = from_txt_label_to_array("./tests/estimation/test_smallerBox.txt")
+    expectedCoordinates = {
+        "x_box_center": 0.6,
+        "y_box_center": 0.5,
+        "width_box": 0.2,
+        "height_box": 0.2,
+    }
+    for key in expectedCoordinates:
+        check.is_true(
+            np.all(np.isclose(expectedCoordinates[key], coord[key])),
+            f"Recreate coordinates {coord[key]} while expecting {expectedCoordinates[key]}.",
+        )
+
+    angles = labels_to_relative_angles(expectedCoordinates)
+    horizontalFOVDegree = 70.42
+    expectedAngles = (0, 0.2 * horizontalFOVDegree)
+
+    check.is_true(
+        np.all(np.isclose(expectedAngles, angles)),
+        f"Received angles {angles} instead of {expectedAngles}",
+    )
