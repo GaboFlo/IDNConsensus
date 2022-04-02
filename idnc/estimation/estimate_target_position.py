edcbc6ee86648929cf202a01b4a6fdd8ea1ae558
Florian Gaboriaud <florian.gaboriaud@polyconseil.fr>
2022-04-02 16:08:43 +0200

    :tada:Minor improvments

--- idnc/estimation/estimate_target_position.py
+++ idnc/estimation/estimate_target_position.py
@@ -1,7 +1,9 @@
-import numpy as np
+import sys
 
 
-def from_txt_label_to_array(relativeTxtPath: str, targetWanted: int = 1) -> list:
+def from_txt_label_to_array(
+    relativeTxtPath: str, targetWanted: int = 1, showPrint: bool = False
+) -> list:
     """
     Open txt file and return the coordinates of the potential box
     Args:
@@ -24,13 +26,15 @@ def from_txt_label_to_array(relativeTxtPath: str, targetWanted: int = 1) -> list
                     "width_box": line[3],
                     "height_box": line[4],
                 }
-                print(f"\nIntruder detected \n{targetCoord}\n")
+                if showPrint:
+                    print(f"\nIntruder detected \n{targetCoord}\n")
     return targetCoord
 
 
 def labels_to_relative_angles(
     coordinates: dict[str, float],
     horizontalFOVDegree: float = 70.42,
+    showPrint: bool = False,
 ):
     """
     Get horizontal angles between the center of the picture and the vertical boxes of the target
@@ -54,16 +58,29 @@ def labels_to_relative_angles(
     relativeLeftAngle = (leftEdgeCoord - 0.5) * horizontalFOVDegree
     """ θ2 """
     relativeRightAngle = (rightEdgeCoord - 0.5) * horizontalFOVDegree
-
-    print(
-        f"\nIntruder detected: \nrelativeLeftAngle (θ1) : {relativeLeftAngle} \nrelativeRightAngle (θ2) : {relativeRightAngle}"
-    )
+    if showPrint:
+        print(
+            f"\nIntruder detected: \nrelativeLeftAngle (θ1) : {relativeLeftAngle} \nrelativeRightAngle (θ2) : {relativeRightAngle}"
+        )
     return (relativeLeftAngle, relativeRightAngle)
 
 
 if __name__ == "__main__":
-    fileName = "test_fullBox.txt"
-    print(f"===={fileName}=====")
+    """_summary_
+
+    Raises:
+        Exception: if no file name is provided with -file argument (.txt label file)
+    """
+
+    args = sys.argv[1:]
+    fileName = ""
+    if len(args) > 1:
+        if args[0] == "-file":
+            fileName = args[1]
+
+    if fileName == "":
+        raise Exception("No label file provided, use ` -file myFileName.txt `")
+
+    print(f"\n===={fileName}=====")
     coord = from_txt_label_to_array(fileName)
     angles = labels_to_relative_angles(coord)
-    print("\n=========\n")
